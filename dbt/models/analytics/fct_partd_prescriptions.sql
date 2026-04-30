773680887e4f1b93572ea2f7c3355cc4c7c37681
Luciana Ferreira da Rocha <lucianaferreiradarocha@gmail.com>
2026-04-30 19:53:44 +0000

    Restructure dbt project into /dbt directory and align GitHub integration

---
+++ dbt/models/analytics/fct_partd_prescriptions.sql
@@ -0,0 +1,19 @@
+SELECT
+    prscrbr_npi,
+    prscrbr_last_org_name,
+    prscrbr_first_name,
+    prscrbr_city,
+    prscrbr_state_abrvtn,
+
+    brnd_name,
+    gnrc_name,
+
+    total_claims,
+    total_beneficiaries,
+    total_drug_cost,
+
+    total_drug_cost / NULLIF(total_claims, 0) AS avg_cost_per_claim,
+
+    report_year
+
+FROM {{ ref('stg_partd_prescribers') }}
